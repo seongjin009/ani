ed6fc7bdbd82306ebcd31075dca531f111cd3ab4
seongjin009 <iseongjin009@gmail.com>
2023-09-27 13:40:28 +0900

    박스 ainme 이용해서 움직이기

--- main.js
+++ main.js
@@ -1,26 +1,16 @@
-/*
-  window/scrollY : 브라우저 스크롤시 스크롤되는 거리값 (동적)
-  DOM.offsetTop : 전체 페이지에 특정 DOM의 세로 위치값 (정적)
-*/
+const box = document.querySelector('article');
 
-const secs = document.querySelectorAll('section');
-const h1 = secs[0].querySelector('h1');
-const h2 = secs[1].querySelector('h1');
-//baseLine은 박스의 모션 시작시점을 기존 박스 영역에서 300만큼 위로 올림
-const baseLine = -window.innerHeight / 2;
-
-const initScroll = 0;
-
-window.addEventListener('scroll', () => {
-	h1.style.transform = `translateX(${setScroll(secs[0])}px)`;
-	h2.style.transform = `translateX(${setScroll(secs[1], -window.innerHeight / 2) / 2}px)`;
+box.addEventListener('click', () => {
+	console.log('test');
+	new Anime(
+		box,
+		{ left: 300, backgroundColor: '#ff69b4' },
+		{
+			duration: 1000,
+			easeType: 'linear',
+			callback: () => {
+				new Anime(box, { top: 300, backgroundColor: '#000000' });
+			},
+		}
+	);
 });
-
-function setScroll(frame, baseLine = 0) {
-	const scroll = window.scrollY;
-	let scroll2 = 0;
-	scroll >= frame.offsetTop + baseLine
-		? (scroll2 = scroll - frame.offsetTop - baseLine)
-		: (scroll2 = 0);
-	return scroll2;
-}
---
+++ setScroll.js
@@ -0,0 +1,32 @@
+/*
+  window/scrollY : 브라우저 스크롤시 스크롤되는 거리값 (동적)
+  DOM.offsetTop : 전체 페이지에 특정 DOM의 세로 위치값 (정적)
+*/
+
+const secs = document.querySelectorAll('section');
+const h1 = secs[0].querySelector('h1');
+const h2 = secs[1].querySelector('h1');
+//baseLine은 박스의 모션 시작시점을 기존 박스 영역에서 300만큼 위로 올림
+let baseLine = -window.innerHeight / 2;
+
+window.addEventListener('scroll', () => {
+	baseLine = -window.innerHeight / 2;
+});
+
+const initScroll = 0;
+
+window.addEventListener('scroll', () => {
+	const scrolled1 = setScroll(secs[0]);
+	const scrolled2 = setScroll(secs[1], baseLine);
+	h1.style.transform = `translateX(${scrolled1}px)`;
+	h2.style.transform = `translateX(${scrolled2}px)`;
+});
+
+function setScroll(frame, baseLine = 0) {
+	const scroll = window.scrollY;
+	let scroll2 = 0;
+	scroll >= frame.offsetTop + baseLine
+		? (scroll2 = scroll - frame.offsetTop - baseLine)
+		: (scroll2 = 0);
+	return scroll2;
+}
