6e756433c2c19bd443bd8acb3b6b7bd1642769f3
seongjin009 <iseongjin009@gmail.com>
2023-09-27 15:32:51 +0900

    버튼빼고 완벽함

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const main = document.querySelector('main');
-const [top, right, bottom, left, open] = main.children;
-const speed = 1000;
+const [top, right, bottom, left, open, content] = main.children;
+const close = content.querySelector('.close');
+const h2 = content.querySelector('h2');
+const speed = 500;
+
+splitText(h2, 0.1, 1);
 
 open.addEventListener('click', () => {
 	new Anime(
@@ -42,3 +46,29 @@ open.addEventListener('click', () => {
 		}
 	);
 });
+
+close.addEventListener('click', () => {
+	content.classList.remove('on');
+
+	setTimeout(() => {
+		new Anime(top, { width: '0%' });
+		new Anime(bottom, { width: '0%' });
+		new Anime(left, { height: '0%' });
+		new Anime(right, { height: '0%' });
+		open.classList.remove('off');
+	}, 1000);
+});
+
+function splitText(selector, interval = 0, delay = 0) {
+	let count = 0;
+	const txt = selector.innerText;
+	selector.innerHTML = '';
+	for (const el of txt) {
+		const span = document.createElement('span');
+		span.innerText = el;
+		span.style.transitionDelay = `${interval * count + delay}s`;
+		span.style.display = 'inline-block';
+		selector.append(span);
+		count++;
+	}
+}
