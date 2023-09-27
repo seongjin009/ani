95f361508aaa60e8a999cc4351ba3110f60e0533
seongjin009 <iseongjin009@gmail.com>
2023-09-27 10:30:25 +0900

    강제보정

--- main.js
+++ main.js
@@ -6,17 +6,28 @@
 const secs = document.querySelectorAll('section');
 const h1 = secs[0].querySelector('h1');
 const h2 = secs[1].querySelector('h1');
+//baseLine은 박스의 모션 시작시점을 기존 박스 영역에서 300만큼 위로 올림
+const baseLine = -300;
+
+const initScroll = 0;
 
 window.addEventListener('scroll', () => {
 	const scroll = window.scrollY;
-	const scroll2 = scroll - secs[1].offsetTop;
+	//박스에 실 적용되는 스크롤 수치값을 반대로 300만큼 더해줘야됨
+	const scroll2 = scroll - secs[1].offsetTop - baseLine;
 	h1.style.transform = `translate(${scroll}px) rotate(${scroll}deg) scale(${1 + scroll / 300})`;
 	h1.style.opacity = 1 - scroll / 300;
 	console.log(scroll);
-	if (scroll > secs[1].offsetTop) {
+	if (scroll > secs[1].offsetTop + baseLine) {
 		h2.style.transform = `translate(${scroll2}px) rotate(${scroll2}deg) scale(${
 			1 + scroll2 / 300
 		})`;
 		h2.style.opacity = 1 - scroll2 / 300;
+	} //원래위치로 스크롤시 조금씩 오차범위가 발행하기 때문에 원래값이 강제 보정처리
+	else {
+		h2.style.transform = `translate(${initScroll}px) rotate(${initScroll}deg) scale(${
+			1 + initScroll / 300
+		})`;
+		h2.style.opacity = 1 - initScroll / 300;
 	}
 });
